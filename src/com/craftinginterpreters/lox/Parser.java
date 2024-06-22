77021b85de2b21e066b31390cee41b00d35d0819
LI Chuan <lichuan172@gmail.com>
2024-06-23 03:26:39 +0800

    feat: add class to jlox

--- src/com/craftinginterpreters/lox/Parser.java
+++ src/com/craftinginterpreters/lox/Parser.java
@@ -29,6 +29,8 @@ public class Parser {
 
   private Stmt declaration() {
     try {
+      if (match(CLASS))
+        return classDeclaration();
       if (match(FUN))
         return function("function");
       if (match(VAR))
@@ -39,6 +41,19 @@ public class Parser {
       synchronize();
       return null;
     }
+  }
+
+  private Stmt classDeclaration() {
+    Token name = consume(IDENTIFIER, "Expect class name.");
+    consume(LEFT_BRACE, "Expect '{' before class body.");
+
+    List<Stmt.Function> methods = new ArrayList<>();
+    while (!check(RIGHT_BRACE) && !isAtEnd()) {
+      methods.add(function("method"));
+    }
+
+    consume(RIGHT_BRACE, "Expect '}' after class body.");
+    return new Stmt.Class(name, methods);
   }
 
   private Stmt statement() {
@@ -202,6 +217,8 @@ public class Parser {
       if (expr instanceof Expr.Variable variable) {
         Token name = variable.name;
         return new Expr.Assign(name, value);
+      } else if(expr instanceof Expr.Get get) {
+        return new Expr.Set(get.object, get.name, value);
       }
 
       // We report an error if the left-hand side of the assignment is not a
@@ -389,6 +406,9 @@ public class Parser {
     while (true) {
       if (match(LEFT_PAREN)) {
         expr = finishCall(expr);
+      } else if (match(DOT)) {
+        Token name = consume(IDENTIFIER, "Expect property name after '.'.");
+        expr = new Expr.Get(expr, name);
       } else {
         break;
       }
@@ -406,6 +426,8 @@ public class Parser {
       return new Expr.Literal(null);
     if (match(NUMBER, STRING))
       return new Expr.Literal(previous().literal);
+    if (match(THIS))
+      return new Expr.This(previous());
     if (match(IDENTIFIER))
       return new Expr.Variable(previous());
     if (match(LEFT_PAREN)) {
